022d90826c71986a17a351eb16116f2c7d8fcdc6
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-12-26 13:17:37 +0500

    Used ExampleLayers/VerletInstancedLayer.h

--- src/Sandbox.cpp
+++ src/Sandbox.cpp
@@ -1,6 +1,17 @@
+#include <Core/EntryPoint.h>
+#include <ExampleLayers/VerletInstancingLayer.h>
 #include "Sandbox.h"
 
+class MyApp : public Application {
+public:
+    MyApp() {
+        PushLayer(new VerletInstanced());
+        PushLayer(new OtherLayer());
+    }
+    ~MyApp() { }
+};
 
-Scene::Scene* CreateScene() {
-    return new Scene::VerletInstanced();
+Application* CreateApplication() {
+    return new MyApp();
 }
+
