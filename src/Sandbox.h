bcce89aa9c14708ea5a4471b1e9726981a9f7741
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-12-28 11:34:29 +0500

    SEGS

--- src/Sandbox.h
+++ src/Sandbox.h
@@ -1,37 +1,52 @@
 #pragma once
-#include <iostream>
-#include <GLBox.h>
+#include "Core/Input.h"
 #include "Core/Layer.h"
+#include "Core/Application.h"
+#include "Core/Instancing/RendererInstanced.h"
+#include "glm/fwd.hpp"
 
+#define PI glm::pi<float>()
+#define TwoPI 2 * glm::pi<float>()
+#define MAXSPEED (float)5
 
 class OtherLayer : public Layer {
 private:
     Pos_Scale_Col_Quad_Manager m_Manager;
-    unsigned int m_O;
+    unsigned int m_Test;
 public:
-    OtherLayer() : Layer("Verlet Test") { }
+    OtherLayer() 
+        : Layer("Other Test")
+    { }
     ~OtherLayer() { }
 
     void OnAttach() override {
-        m_O = m_Manager.AllocateObject(10, &ConfigureShader);
-        m_Manager[m_O].scale = glm::vec2(100);
-        m_Manager[m_O].scale = glm::vec4(1,1,1,1);
-        m_Manager[m_O].position = glm::vec2(100, 100);
-        std::cout << "Attached" << '\n';
+        m_Test = m_Manager.AllocateObject(1, &ConfigureShader);            
+
+        // set graphic for contraint
+        m_Manager[m_Test].position = glm::vec2(WIDTH, HEIGHT)/2.0f;
+        m_Manager[m_Test].scale = glm::vec2(100);
+        m_Manager[m_Test].color = glm::vec4(0,0,0,1);
     }
 
     void OnDetach() override {
-        std::cout << "Detached" << '\n';
+        
     }
+
     void Update(float dt) override {
-        // m_O.position.x += dt;
+        // m_ObjData[0].position = Lerp(m_ObjData[1].position, 
+        //                              glm::vec2(Input::GetMousePos().x, HEIGHT - Input::GetMousePos().y), 10 * dt);
+        m_Manager[m_Test].position = glm::vec2(Input::GetMousePos().x, HEIGHT - Input::GetMousePos().y);
+
+        if (Input::Button(GLFW_MOUSE_BUTTON_LEFT, GLFW_PRESS))
+            m_Manager[m_Test].scale = Lerp(m_Manager[m_Test].scale, glm::vec2(8.0f), dt * 10.0f);
+        else
+            m_Manager[m_Test].scale = Lerp(m_Manager[m_Test].scale, glm::vec2(100.0f), dt * 10.0f);
     }
 
     void Render() override {
     }
 
     void ImGuiRender() override {
-        ImGui::ShowDemoWindow();
     }
 private:
     static void ConfigureShader(InstanceRenderer& renderer) {
@@ -45,3 +60,5 @@ private:
         return a + p * (b-a);
     }
 };
+
+
