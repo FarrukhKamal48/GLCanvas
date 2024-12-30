1adc170e09b07248f9ff58bd1642bc41c04de07d
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-12-30 13:23:28 +0500

    only that latest layer is rendered

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -6,7 +6,7 @@ class MyApp : public Application {
 public:
     MyApp() : Application({ "GLBox", 1920, 1080 })
     {
-        // PushLayer(new VerletInstanced());
+        PushLayer(new VerletInstanced());
         PushLayer(new DynamicLayer());
     }
     ~MyApp() { }
