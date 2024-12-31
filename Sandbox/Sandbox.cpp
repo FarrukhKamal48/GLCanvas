1fa11f9173e527c9627489bcfe7df649f2d191d4
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-12-31 09:17:12 +0500

    all layers now render completely fine

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -6,6 +6,7 @@ class MyApp : public Application {
 public:
     MyApp() : Application({ "GLBox", 1920, 1080 })
     {
+        PushLayer(new SpinLayer());
         PushLayer(new VerletInstanced());
         PushLayer(new DynamicLayer());
     }
--- Sandbox/Sandbox.h
+++ Sandbox/Sandbox.h
@@ -11,35 +11,20 @@
 class DynamicLayer : public Layer {
 private:
     QuadTransform_Manager m_Manager;
-    unsigned int m_Test;
 public:
     DynamicLayer() 
-        : Layer("Other Test")
+        : Layer("Dynamic Test")
     { }
     ~DynamicLayer() { }
 
     void OnAttach() override {
-        m_Test = m_Manager.AllocateObject(1, &ConfigureShader);
-
-        m_Manager[m_Test].position = glm::vec2(WIDTH, HEIGHT)/2.0f;
-        m_Manager[m_Test].scale = glm::vec2(100);
-        m_Manager[m_Test].rotation = PI/4;
-        m_Manager[m_Test].color = glm::vec4(0,0,0,1);
     }
 
     void OnDetach() override {
         
     }
 
-    bool spawn = true;
     void Update(float dt) override {
-        m_Manager[m_Test].position = glm::vec2(Input::GetMousePos().x, HEIGHT - Input::GetMousePos().y);
-        m_Manager[m_Test].rotation += dt * 5;
-
-        if (Input::Button(GLFW_MOUSE_BUTTON_LEFT, GLFW_PRESS))
-            m_Manager[m_Test].scale = Lerp(m_Manager[m_Test].scale, glm::vec2(8.0f), dt * 10.0f);
-        else
-            m_Manager[m_Test].scale = Lerp(m_Manager[m_Test].scale, glm::vec2(100.0f), dt * 10.0f);
     }
 
     void Render() override {
@@ -55,7 +40,7 @@ public:
             m_Manager[objI].position = glm::vec2(inc * 50 * glm::sin(inc) + (float)WIDTH/2,
                                                  inc*5);
             m_Manager[objI].scale = glm::vec2(20);
-            m_Manager[objI].rotation = m_Manager[m_Test].rotation;
+            m_Manager[objI].rotation = inc*glm::sin(inc*20);
             m_Manager[objI].color = glm::vec4(1, 0.5, 0, 1);
         }
         ImGui::End();
@@ -72,3 +57,52 @@ private:
 };
 
 
+class SpinLayer : public Layer {
+private:
+    QuadTransform_Manager m_Manager;
+    unsigned int m_Test;
+public:
+    SpinLayer() 
+        : Layer("Spin Test")
+    { }
+    ~SpinLayer() { }
+
+    void OnAttach() override {
+        m_Test = m_Manager.AllocateObject(1, &ConfigureShader);
+
+        m_Manager[m_Test].position = glm::vec2(WIDTH, HEIGHT)/2.0f;
+        m_Manager[m_Test].scale = glm::vec2(0);
+        m_Manager[m_Test].rotation = PI/4;
+        m_Manager[m_Test].color = glm::vec4(0,0,0,1);
+    }
+
+    void OnDetach() override {
+        
+    }
+
+    bool spawn = true;
+    void Update(float dt) override {
+        m_Manager[m_Test].position = glm::vec2(Input::GetMousePos().x, HEIGHT - Input::GetMousePos().y);
+        m_Manager[m_Test].rotation += dt * 5;
+
+        if (Input::Button(GLFW_MOUSE_BUTTON_LEFT, GLFW_PRESS))
+            m_Manager[m_Test].scale = Lerp(m_Manager[m_Test].scale, glm::vec2(8.0f), dt * 10.0f);
+        else
+            m_Manager[m_Test].scale = Lerp(m_Manager[m_Test].scale, glm::vec2(50.0f), dt * 10.0f);
+    }
+
+    void Render() override {
+    }
+
+    void ImGuiRender() override {
+    }
+private:
+    static void ConfigureShader(InstanceRenderer& renderer) {
+        renderer.CreateShader("GLBox/assets/shaders/instancing/RotationQuad.vert", 
+                              "GLBox/assets/shaders/instancing/RotationQuad.frag");
+    }
+
+    glm::vec2 Lerp(glm::vec2 a, glm::vec2 b, float p) {
+        return a + p * (b-a);
+    }
+};
