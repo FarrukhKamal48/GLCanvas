846c52421b92e7cae7055e4200d2c083d0691332
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-01-05 14:26:40 +0500

    Added custom arithmatic operator definitions for ImVec2

---
+++ GLCanvas/ImVec2Extend.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <GLBox.h>
+
+template<typename TpA, typename TpB>
+inline ImVec2 operator+(TpA A, TpB B) { return ImVec2(A.x+B.x, A.y+B.y); }
+
+template<typename TpA, typename TpB>
+inline ImVec2 operator-(TpA A, TpB B) { return ImVec2(A.x-B.x, A.y-B.y); } 
+
+template<typename T>
+inline ImVec2 operator/(T vec, float val) { return ImVec2(vec.x/val, vec.y/val); }
+
+template<typename T>
+inline ImVec2 operator*(T vec, float val) { return ImVec2(vec.x*val, vec.y*val); }
