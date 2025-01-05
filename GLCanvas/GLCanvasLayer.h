961da77ba41ed28cf9cbdf627f01d2b444c3f732
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-01-05 13:47:29 +0500

    Moved calculation of m_WorlMousePos to OnUpdate function

--- GLCanvas/GLCanvasLayer.h
+++ GLCanvas/GLCanvasLayer.h
@@ -57,22 +57,26 @@ public:
     }
 
     void OnUpdate(float dt) override {
+        // take windowMousePos and convert to coords in camera space and then translate my camera's position
+        m_WorldMousePos = glm::vec2(
+            (m_WindowMousePos.x/m_ViewportSize.x - 0.5f) * m_CameraController.GetBounds().x,
+            (1.0f - m_WindowMousePos.y/m_ViewportSize.y - 0.5f) * m_CameraController.GetBounds().y
+        ) + glm::vec2(m_CameraController.GetCamera().GetPosition());
+        
+        static glm::vec2 lastPos = { 0, 0 };
+        m_MouseDelta = m_WorldMousePos - lastPos;
+        lastPos = m_WorldMousePos;
+        
+        m_Manager[m_Test].position = glm::vec3(m_WorldMousePos, 0.0f);
+        m_Manager[m_Test].rotation += 5 * dt;
+
         if (Input::KeyPressed(Key::Space))
             m_Manager[m_Test].scale = Lerp(m_Manager[m_Test].scale, glm::vec2(0.01f), dt * 10.0f);
         else
             m_Manager[m_Test].scale = Lerp(m_Manager[m_Test].scale, glm::vec2(0.05f), dt * 10.0f);
         
-        static glm::vec2 lastPos = { 0, 0 };
-        m_MouseDelta = m_WindowMousePos - lastPos;
-        lastPos = m_WindowMousePos;
-
-        m_WorldMousePos = m_WindowMousePos + glm::vec2(m_CameraController.GetCamera().GetPosition());
-        
         if (Input::KeyPressed(Key::LeftAlt) && Input::MousePressed(Mouse::ButtonLeft))
             m_CameraController.Translate(-glm::vec3(m_MouseDelta, 0.0f));
-        
-        m_Manager[m_Test].position = glm::vec3(m_WorldMousePos, 0.0f);
-        m_Manager[m_Test].rotation += 5 * dt;
 
         m_ColorCard.SetMousePos(m_WorldMousePos);
         m_ColorCard.SetMouseDelta(m_MouseDelta);
@@ -98,36 +102,17 @@ public:
             }
             ImGui::Image(m_Framebuffer.GetColorAttachment(), viewportSize, ImVec2(0,1), ImVec2(1,0));
             
-            // static ImVec2 drawpos = { 0, 0 };
-            // ImVec2 wishdir = { 0, 0 };
-            // if (Input::KeyPressed(Key::W))      wishdir.y = -1;
-            // else if (Input::KeyPressed(Key::S)) wishdir.y = 1;
-            // else                                wishdir.y = 0;
-            //
-            // if (Input::KeyPressed(Key::D))      wishdir.x = 1;
-            // else if (Input::KeyPressed(Key::A)) wishdir.x = -1;
-            // else                                wishdir.x = 0;
-            //
-            // drawpos.x += wishdir.x * 10.0f;
-            // drawpos.y += wishdir.y * 10.0f;
-            //
-            // ImGui::GetWindowDrawList()->AddCircleFilled(drawpos, 10.0f, IM_COL32_WHITE);
-                
             m_WindowMousePos = { 
                 ImGui::GetMousePos().x - ImGui::GetWindowPos().x, 
                 ImGui::GetMousePos().y - ImGui::GetWindowPos().y 
             };
-            m_WindowMousePos = glm::vec2(
-                (m_WindowMousePos.x/m_ViewportSize.x - 0.5f) * m_CameraController.GetBounds().x,
-                (1.0f - m_WindowMousePos.y/m_ViewportSize.y - 0.5f) * m_CameraController.GetBounds().y
-            );
-
+            
             m_ColorCard.OnImGuiRender();
         }
         ImGui::End();
         ImGui::PopStyleVar();
 
-        ImGui::Begin("Settings"); 
+        ImGui::Begin("Settings", nullptr, ImGuiWindowFlags_NoTitleBar); 
         {
             ImGui::Button("Hell");
         }
