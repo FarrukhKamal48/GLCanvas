9b21633b8e291c9d794f68d1b3152a2eb48a7d42
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-01-12 15:34:14 +0500

    Moved each canvas state class to its own file

---
+++ GLCanvas/States/IdleState.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <GLBox.h>
+#include "GLCanvas/Canvas/Canvas.h"
+
+namespace Canvas {
+
+class IdleState : public BaseState {
+public:
+    IdleState(StateKey state) : BaseState(state) {}
+    ~IdleState() {}
+    
+    void OnEnter() override { BASIC_LOG("Idle"); }
+    void OnEvent(Event& event) override {} 
+    void OnUpdate(float dt) override { } 
+    void OnExit() override {} 
+    StateKey GetNextState() override { 
+        if (Input::KeyPressed(Key::Space))
+            return State::Panning; 
+        return State::Idle;
+    }
+};
+
+}
---
+++ GLCanvas/States/PanningState.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <GLBox.h>
+#include "GLCanvas/Canvas/Canvas.h"
+
+namespace Canvas {
+
+class PanningState : public BaseState {
+public:
+    PanningState(StateKey state) : BaseState(state) {}
+    ~PanningState() {}
+    
+    void OnEnter() override { BASIC_LOG("Panning"); }
+    void OnEvent(Event& event) override {} 
+    void OnUpdate(float dt) override { } 
+    void OnExit() override {} 
+    StateKey GetNextState() override { 
+        if (Input::KeyPressed(Key::Enter))
+            return State::Idle; 
+        return State::Panning;
+    }
+};
+
+}
