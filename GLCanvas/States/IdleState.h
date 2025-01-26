243fe22949459904a72ad2ca27f68115feb6d501
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-01-26 13:39:00 +0500

    State transitions enter a cyclical loop between Idle and DraggCard states when creating cards through the 'Cards' window

--- GLCanvas/States/IdleState.h
+++ GLCanvas/States/IdleState.h
@@ -9,24 +9,22 @@ public:
     IdleState() : State(StateType::Idle) {}
     ~IdleState() {}
     
-    void OnEnter() override { }
+    void OnEnter() override {  }
     void OnEvent(Event& event) override { } 
     void OnUpdate(float dt) override { } 
     void OnImGuiRender() override {
-        m_IsCanvasHovered = ImGui::IsWindowHovered();
+        m_IsCanvasHovered = ImGui::GetIO().WantCaptureMouse;
+        BASIC_LOG(m_IsCanvasHovered);
     }
     void OnExit() override { } 
     StateKey GetNextState() override { 
-        if (!m_IsCanvasHovered)
-            return StateType::Idle;
-        
-        if (Input::MousePressed(Mouse::ButtonLeft) && Input::KeyPressed(Key::LeftAlt)) {
+        if (Input::MousePressed(Mouse::ButtonLeft) && Input::KeyPressed(Key::LeftAlt) && m_IsCanvasHovered) {
             return StateType::Panning;
         }
         else if (Input::MousePressed(Mouse::ButtonLeft) && CVData().Cardmanager->IsValid(CVData().HoveredCardID)) {
             return StateType::DraggCard;
         }
-        if (Input::MousePressed(Mouse::ButtonRight) && !CVData().Cardmanager->IsValid(CVData().HoveredCardID)) {
+        if (Input::MousePressed(Mouse::ButtonRight) && !CVData().Cardmanager->IsValid(CVData().HoveredCardID) && m_IsCanvasHovered) {
             return StateType::CreateCard;
         }
         return StateType::Idle;
