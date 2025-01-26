c84ea9f9a9a5a1c1fb49377549a4321bdcd8904d
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-01-26 14:35:06 +0500

    Removed window checks temporarily

--- GLCanvas/States/IdleState.h
+++ GLCanvas/States/IdleState.h
@@ -12,25 +12,20 @@ public:
     void OnEnter() override {  }
     void OnEvent(Event& event) override { } 
     void OnUpdate(float dt) override { } 
-    void OnImGuiRender() override {
-        m_IsCanvasHovered = ImGui::GetIO().WantCaptureMouse;
-        BASIC_LOG(m_IsCanvasHovered);
-    }
+    void OnImGuiRender() override { }
     void OnExit() override { } 
     StateKey GetNextState() override { 
-        if (Input::MousePressed(Mouse::ButtonLeft) && Input::KeyPressed(Key::LeftAlt) && m_IsCanvasHovered) {
+        if (Input::MousePressed(Mouse::ButtonLeft) && Input::KeyPressed(Key::LeftAlt)) {
             return StateType::Panning;
         }
         else if (Input::MousePressed(Mouse::ButtonLeft) && CVData().Cardmanager->IsValid(CVData().HoveredCardID)) {
             return StateType::DraggCard;
         }
-        if (Input::MousePressed(Mouse::ButtonRight) && !CVData().Cardmanager->IsValid(CVData().HoveredCardID) && m_IsCanvasHovered) {
+        if (Input::MousePressed(Mouse::ButtonRight) && !CVData().Cardmanager->IsValid(CVData().HoveredCardID)) {
             return StateType::CreateCard;
         }
         return StateType::Idle;
     }
-private:
-    bool m_IsCanvasHovered = false;
 };
 
 }
